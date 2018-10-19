0f07f0b4772daef9e0d89118738015c5ce53fe60
Vladimir Kasatkin <torwald@yandex.ru>
2018-10-19 20:51:15 +0700

    Add the second button

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         flipCard(withEmoji: "☆", on: sender)
     }
 
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "☪︎", on: sender)
+    }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if (button.currentTitle == emoji) {
             button.setTitle("", for: UIControl.State.normal)
